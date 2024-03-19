739c505e275755c9dc53de1a69dd90ddd8bc31c1
abrarmahin03 <163482134+abrarmahin03@users.noreply.github.com>
2024-03-18 21:58:24 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,10 +2,10 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "RHEL-Server" {
+resource "aws_instance" "RHEL-Service" {
   count         = 1
   ami           = "ami-0fe630eb857a6ec83"
-  instance_type = "t2.micro"
+  instance_type = "t2.meduim"
   key_name      = "Laptop Keypair"
   tags = {
     Name = "RHEL-Server"
